7f8561a34d9bab03db4bf75db8c75a11b10f0a1e
Melany-Chairez-1058 <164534062+Melany-Chairez-1058@users.noreply.github.com>
2025-02-07 16:47:40 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+// funcion que no retorna nada
+//funcion saludar
+
+void saludar(){
+  print("Hola Mundo");
+  
+}
+void escribeNombre(){
+  print("Melany Sarahi Chairez Arias");
+}
+void main(){
+  // Melany Sarahi Chairez Arias 22308051058
+  //llama a la funcion saludar
+  print("Melany Sarahi Chairez Arias 22308051058");
+  print("Llamando a la funcion saludar");
+  saludar();
+  print("Llamando a la funcion escribeNombre");
+  escribeNombre();
+}
