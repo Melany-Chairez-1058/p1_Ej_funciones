c989bd0b44f9abab36de5d16e154c184d8fe86fd
Melany-Chairez-1058 <164534062+Melany-Chairez-1058@users.noreply.github.com>
2025-02-11 20:38:36 +0000

    Problema_6

--- problema6.dart
+++ problema6.dart
@@ -1,21 +1,46 @@
-void mostrarYCalcularProducto(List<double> lista) {
+import 'dart:io';
+
+// Función para capturar los datos de la lista
+List<double> capturarDatos() {
+  List<double> lista = [];
+  print("Ingrese la cantidad de elementos de la lista:");
+  int cantidad = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < cantidad; i++) {
+    print("Ingrese el elemento ${i + 1}:");
+    double elemento = double.parse(stdin.readLineSync()!);
+    lista.add(elemento);
+  }
+
+  return lista;
+}
+
+// Función para mostrar los elementos de la lista
+void mostrarElementos(List<double> lista) {
   print("\nElementos de la lista:");
   for (int i = 0; i < lista.length; i++) {
     print("Elemento ${i + 1}: ${lista[i]}");
   }
+}
 
-  // Calcular el producto de los elementos
+// Función para calcular el producto de los elementos de la lista
+double calcularProducto(List<double> lista) {
   double producto = 1.0; // Inicializamos con 1 porque es el elemento neutro de la multiplicación
   for (int i = 0; i < lista.length; i++) {
     producto *= lista[i];
   }
-
-  print("\nEl producto de todos los elementos de la lista es: $producto");
+  return producto;
 }
+
 void main() {
-  // Lista de valores fijos (predefinidos)
-  List<double> numeros = [2.5, 3.0, 4.0,5.6,22.14];
- print("Melany Sarahi Chairez Arias 22308051058");	
-  // Mostrar los elementos de la lista y calcular el producto
-  mostrarYCalcularProducto(numeros);
+  // Capturar los datos de la lista
+  List<double> numeros = capturarDatos();
+
+  // Mostrar los elementos de la lista
+  mostrarElementos(numeros);
+
+  // Calcular el producto de los elementos
+  double producto = calcularProducto(numeros);
+  print('melany sarahi chairez arias 22308051058');
+  print("\nEl producto de todos los elementos de la lista es: $producto");
 }
--- recursos/problema6.md
+++ recursos/problema6.md
@@ -1,2 +1,3 @@
+* 7 Implementa un programa que tome una lista de cadenas (string) y cuente  cuántas palabras hay en total en todas las cadenas.
 * Salida:
-* ![alt text](image-5.png)
+* ![alt text](image-7.png)
